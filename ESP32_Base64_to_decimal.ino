8b59742f360675346594a58ff9712e4c12e76934
my-dudhwala <86928659+my-dudhwala@users.noreply.github.com>
2023-12-19 11:45:07 +0530

    Create ESP32_Base64_to_decimal.ino

---
+++ ESP32_Base64_to_decimal.ino
@@ -0,0 +1,172 @@
+//Strings to convert from base64 to decimal
+String array1 = "dHAndMGlIWfNcEXxxPhhIcjcDBJFzeXoUPVWoOZFAIZVVDmOnuRNNWacFmvOyAlCtUwSWPbrQjRixHsAHS\\nHiHvhalYMxoGmViDfWyylQbhkfLVOLVjwPQdANnZBrCIyVxHQHiDQXfTqQfTkfYUwhuRIGQzaukWVCZniofnqrJBvBVFhvEByOTteRXIbfXBAISVSUcNRZPXaTqoTJVFxWBXkypxvsq\\nRGKnfufQrtFSVuXsSYbDhPIuTRSEZEJvALbkyUtauFfvGoyYLnoEwUmJCHHOhpcYBQEfgAFAerenyCGnOktJNbnEZEMGmqKoozgq\\nIbtUwfGuJXdYIEhhxkxwTHacVugEVMkhKJJaPNgOJpODdMszSeMjwwIHXUWgwhbhwptljVptmeFmeDMrBndYVVlbifpElueFayYS\\nuJsAPZuGNCxFlJWdDdwxMmxzEcHNwvfFoXSJPolQgnxxNEJEVWCeMNkAhnnHkakHIfyVbhPHfOAEmjiZniZxAaETjpOrHZxMFgvdgmlcdEtKMn\\nTDDjaziWjYhRISEXBzudTJGFqlaUUFyIjezFoWDaQGhUGKtNWCtbTyUIZNcHTDWGZdPCtuYwtrwNIsPvVmaqSCyNlTpOQEwTRtAGXXiNcsXmp\\nKUyTjSYAvGLjLoTtcMVJzFjKktufNKuXACpxYReUiwjWoAnyytDparBWNAcqUCTqNKjqNfszoiTaCaPEnrnuVZhFiQruoNbrRNEQiefiiaQPfz\\nQisJfgdJjrbWbKRrZbhnNrrdrblVXEAwkftaYwWegnFuaJdNgiOYYgnuVWmwrSxhQZuHuizCGVfOetNKEvLkGahHPWrFPoGYvGWPhXhyEzs\\nKuBNQDJGqWenZbzMRLtogMDuZQXPORTZXrecTDNqqNruEbiLIReyPPAfKONzXfRandjbTBCXFnNhZLyChQixAkSuTOAcFXWKimlkZRDzAGqu\\nRwcuchClbmiFHfazKYCcMNpkZnXvhldRIrzlLxOpVqMZchnZmXPERlKwvRjJBeUxINkijjcvroPJyTCkZGnpMzKgSnXNmqTAFlUgSRpDTyCKcf\\nWMDqJaryVOTMRVdzOeWmldShlilsgGmtZHSGdpOYGdmFhWBXNTnjoTQeDeRHkasVHlaikXYWuyfBOijdMJYhRXfPXXqLtHYQBPxEEjKrW\\nqaHrDLTziRBMvznftcbmjkPBHYfhDDdmnsYGlrpzLJCznAUjvOjStfLRPrVbTtnAeNnbkzbtRvYnSnvUOgeDuINMYnpHmpkEFBdFPXnjGbvcoDa\\nUJXomVPTyHcxMcDGGXjjKHlPihjefvpQuhqrmVxpAjMjgzZGtEPJRGrkvblpIFHZAVgPaRQITjgkmhsbjooTyEGShCrblBur\\nubnJmgkKTTRGonzxikmgyxGXLxEdkuTNoyMggeObbvcQqDmovPLjohhXksNMpEvwWABaVmYqfmmaCLFHuboSVnmmbdQJEIVnhfAeUqwJjMjtg\\nOOfcafuddgiSxbavUVVEptTrltCPYuysHsBIXWjOrTEpHSDNsNZgKEgaqgsJUSaw\\npmCmkMaRrSRktotvGoSvWwDJMYRqQHpVLZRfdKgswCEZzvVZJtKexCbvFYdXISckfVbGbqBXNtDopMCTTosxogflBllsXIKuQhyRKGKFGRSFzFdI\\nbNjGVxfWBTGmeuFVaYvZkONXNtcJoMNxuiRPJKmlRfNkEIaaYaxdgCuHdQUcUGvDwiVbvdumYRgTTHlHkihXPdmPRlqEbiKeAZhQoyHVQXgWWFZ\\ncEUoeeMDSYNxWbNtvVzVmAnyvJJnYBkBOiCwlgWORxgBPrXevQlqebASiqnbTsdAAYuUTkASSvfSKxyBrIEAjuPbEINrOPDZDhhUeFuxaMHuJHTFP\\nHRHLOHiwWBkaNRKpDnLtDVqrMugqlspfAEXwyYtXdBgUhvXsKVkCWNvJregpjeHRBUcjQMPfOfONJXrfXUATxzUuJlDilXAHzmJVnMwKsyegHffggD\\nSNKAAmYpRWiRmzPpFucdeIrBCHNTurxKAJGGxDmjdoMeebbhTeBzMMEtPqwdqNCREjUuhhUPEgreUMyvQwhnKcADJxOmUzWwiklkyjVQWXuVxb\\n";
+String array3 = "MLRlXfZhYiIAogbliTGlgrvysrSHSCdlTZamSutEgGZpNzfmhMqzBdieOygAkDPjmrojYlKXdEbKhnCYhD\\nMXAWCtmsRjNsGKlurfLJEuwhtIYOlTbBAyWshGUqbBZpPfPKKnDTAXWHRWlbVsNleTclmSYJqwxcPsrNYGtBJywlNWsjwfCtkkxnhdwwuxVMInwXKRUhyOhLNjomBppPPRvsgiIvhWz\\nkLfPZkdnzncAWzeyYYJLAurhCyrKHlEihekGOYopiyCyXzcLpGZqzDnToDCExPInuKZiOlTSVejqwXnhElOPwEnnecLflfzKYGRY\\nOhiFlbgCtQGLReFWegAoWMYwyZDhxstfTuQJAgPzuLDAYbtXSYbiTbhwJEMcwmZygCszCBZqkJySlAiKToceYXKfMOGPuWrkZUhW\\naUVRQjuLRlVPOGbWafMZLdDoJSFqqnTsApKVLlkuWgfGxuMbBINqUsjZvaPrNhktxHsWkqKWcVLqggOmaRWVanyriCrjMxNdwVROgsTmBZIanM\\nXdlIBjbhghjHdtVCEjZIfxfhPAyrzxEXVazLpMUPQXCTDYjqyvikiLtZQAoHSBaCnUYMzSfYdiIURFqodOWYbQejuRHYnyFawEaEiXZaBcETb\\nfXvUoswKDdbLFIZkpHYSqgCjYTeAzUtmHeaMSFIPdTeGEFAwccQQfmhVtbEagRmoEwnfkCmamjZKYwIznEifelnAPSWrAYBjTiTyJraPyjsNHs\\nvPqRvEVznqUcydBvuonABuAtboWHKWdckmgGbnydnBRsmGQwlvPhazWcHFyAcgbACrjqbKlTMDxjPWMQGsoEEYOXeYUfAvwEmfgsqiNIHsd\\nZsLMMRpZBUPbrqJlcxojxzZwgzXntTjFRPaVkpqYsOAfOVYBGnthxIAjHbwWebSPYmLDmsNdKBjeipZUZpnfDZtkwdyXhkIYjyNbwctJwPzT\\nxkEAwLGOmkHkjKOzQtiIrJorXFNYqucCFIGSmMhDLAXmNkcEzjIvwizqYMVbAlBqcaqnCNRguHzzBKcXyIlBnzUoxCxpJYHWhaoErrIFGaVEdM\\nhCVhlervzNeqjCwskPwHWCdYmOXwLGJncoxcdmlepBzIbuxEjVUzJsXCUGhSCYohoCLDFAfvRcRNLQxRLFRKZNiaLpQVogLlBMTccYBdP\\nQoDFOZnwhxlktIDCwmZqVGkzuWmLhjpJroUZZrADUsWvdDKecKzWBkwZYgepuWGZBEPjHIDtGbwwogMWJdvfKDtapUxOsMxIAkwXJjIxxAVwxFS\\nsFOApBdknfJIUKyKMENPiQZwCNhtPoRmnqLwkLAyPEkPOIPRFDeFymbdebcRDPyFXsXvjOByodxxoLDyzLoKtwDNMtpPqDoJ\\nZTGSvIFkFjRaSpgigOAxSXEKrQmzHOIbpjFLjECAuWLbhaSutNyZOyGnZGZeBkjdHUttkwpNMcOqLPGeEdLrrwluNbbDkdnSlcVXQMYwhRWge\\nkeVXTdTOUSWxDKsXZhNvgTXTzgZEIiCBASOpzLBjYMASeOECCEQmBlfbETildpKf\\nbhXsNiDjGALdXenMxCHjpgUYodykEyJuKtHlieZIhmOODNEySTEiaoToKrbaKNJyYyJrUuVyQoyoxeERHXWDQvIpMStOVoUHPktxkKsqNhnYfiyd\\nmLOQgUJmgDMBMDzAmAViFJjSHZpfInpqtMLDGhXfCGIMvjNQGUeBocEEUAhizHxnqapbQvVfkxdlsXjQDbWNibKmEtpcftdVxqEPSYVTsFDWDsb\\nUfHthwyNFuGWxPWCbuLoekRLJIlWUVIuEPKbauDuLLuVymykVLkBnuSugIpkVkswwEaQISGinrKPMfkXiicyfEEsRdyaWSlUCSLaQdAWWYCLPoNnX\\nuGIPtPQUAFitKaAvkPylCiupFRoNvmvJkeKmjnBMUjYcYRZPcvDnkFgqpBQUvWgmxsAeNNzpZPPTJwCIHHryFiXRXifJfCquTmXlMdArKoZreANlyd\\nvqldHhyYWWXGUmMJfZQVrmhILvYHNdzBgSbIGCkqMvStuLVtxZvNwkoUiizVAKRJhpftglROIpcUeXCdUihYmbSySdtxvVzMfWdOAFJkzggtAF\\n";
+
+//Base64 char array to compare, and its index is according to Base64 table indexes
+char array2[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";//This is characr
+
+//Random data from https://github.com/my-dudhwala/ESP_Big_Data/blob/main/Sample_data/data1to375.txt
+//"ID_one" : "dHAndMGlIWfNcEXxxPhhIcjcDBJFzeXoUPVWoOZFAIZVVDmOnuRNNWacFmvOyAlCtUwSWPbrQjRixHsAHSHiHvhalYMxoGmViDfWyylQbhkfLVOLVjwPQdANnZBrCIyVxHQHiDQXfTqQfTkfYUwhuRIGQzaukWVCZniofnqrJBvBVFhvEByOTteRXIbfXBAISVSUcNRZPXaTqoTJVFxWBXkypxvsqRGKnfufQrtFSVuXsSYbDhPIuTRSEZEJvALbkyUtauFfvGoyYLnoEwUmJCHHOhpcYBQEfgAFAerenyCGnOktJNbnEZEMGmqKoozgqIbtUwfGuJXdYIEhhxkxwTHacVugEVMkhKJJaPNgOJpODdMszSeMjwwIHXUWgwhbhwptljVptmeFmeDMrBndYVVlbifpElueFayYSuJsAPZuGNCxFlJWdDdwxMmxzEcHNwvfFoXSJPolQgnxxNEJEVWCeMNkAhnnHkakHIfyVbhPHfOAEmjiZniZxAaETjpOrHZxMFgvdgmlcdEtKMnTDDjaziWjYhRISEXBzudTJGFqlaUUFyIjezFoWDaQGhUGKtNWCtbTyUIZNcHTDWGZdPCtuYwtrwNIsPvVmaqSCyNlTpOQEwTRtAGXXiNcsXmpKUyTjSYAvGLjLoTtcMVJzFjKktufNKuXACpxYReUiwjWoAnyytDparBWNAcqUCTqNKjqNfszoiTaCaPEnrnuVZhFiQruoNbrRNEQiefiiaQPfzQisJfgdJjrbWbKRrZbhnNrrdrblVXEAwkftaYwWegnFuaJdNgiOYYgnuVWmwrSxhQZuHuizCGVfOetNKEvLkGahHPWrFPoGYvGWPhXhyEzsKuBNQDJGqWenZbzMRLtogMDuZQXPORTZXrecTDNqqNruEbiLIReyPPAfKONzXfRandjbTBCXFnNhZLyChQixAkSuTOAcFXWKimlkZRDzAGquRwcuchClbmiFHfazKYCcMNpkZnXvhldRIrzlLxOpVqMZchnZmXPERlKwvRjJBeUxINkijjcvroPJyTCkZGnpMzKgSnXNmqTAFlUgSRpDTyCKcfWMDqJaryVOTMRVdzOeWmldShlilsgGmtZHSGdpOYGdmFhWBXNTnjoTQeDeRHkasVHlaikXYWuyfBOijdMJYhRXfPXXqLtHYQBPxEEjKrWqaHrDLTziRBMvznftcbmjkPBHYfhDDdmnsYGlrpzLJCznAUjvOjStfLRPrVbTtnAeNnbkzbtRvYnSnvUOgeDuINMYnpHmpkEFBdFPXnjGbvcoDaUJXomVPTyHcxMcDGGXjjKHlPihjefvpQuhqrmVxpAjMjgzZGtEPJRGrkvblpIFHZAVgPaRQITjgkmhsbjooTyEGShCrblBurubnJmgkKTTRGonzxikmgyxGXLxEdkuTNoyMggeObbvcQqDmovPLjohhXksNMpEvwWABaVmYqfmmaCLFHuboSVnmmbdQJEIVnhfAeUqwJjMjtgOOfcafuddgiSxbavUVVEptTrltCPYuysHsBIXWjOrTEpHSDNsNZgKEgaqgsJUSawpmCmkMaRrSRktotvGoSvWwDJMYRqQHpVLZRfdKgswCEZzvVZJtKexCbvFYdXISckfVbGbqBXNtDopMCTTosxogflBllsXIKuQhyRKGKFGRSFzFdIbNjGVxfWBTGmeuFVaYvZkONXNtcJoMNxuiRPJKmlRfNkEIaaYaxdgCuHdQUcUGvDwiVbvdumYRgTTHlHkihXPdmPRlqEbiKeAZhQoyHVQXgWWFZcEUoeeMDSYNxWbNtvVzVmAnyvJJnYBkBOiCwlgWORxgBPrXevQlqebASiqnbTsdAAYuUTkASSvfSKxyBrIEAjuPbEINrOPDZDhhUeFuxaMHuJHTFPHRHLOHiwWBkaNRKpDnLtDVqrMugqlspfAEXwyYtXdBgUhvXsKVkCWNvJregpjeHRBUcjQMPfOfONJXrfXUATxzUuJlDilXAHzmJVnMwKsyegHffggDSNKAAmYpRWiRmzPpFucdeIrBCHNTurxKAJGGxDmjdoMeebbhTeBzMMEtPqwdqNCREjUuhhUPEgreUMyvQwhnKcADJxOmUzWwiklkyjVQWXuVxb",
+//"ID_two" : "MLRlXfZhYiIAogbliTGlgrvysrSHSCdlTZamSutEgGZpNzfmhMqzBdieOygAkDPjmrojYlKXdEbKhnCYhDMXAWCtmsRjNsGKlurfLJEuwhtIYOlTbBAyWshGUqbBZpPfPKKnDTAXWHRWlbVsNleTclmSYJqwxcPsrNYGtBJywlNWsjwfCtkkxnhdwwuxVMInwXKRUhyOhLNjomBppPPRvsgiIvhWzkLfPZkdnzncAWzeyYYJLAurhCyrKHlEihekGOYopiyCyXzcLpGZqzDnToDCExPInuKZiOlTSVejqwXnhElOPwEnnecLflfzKYGRYOhiFlbgCtQGLReFWegAoWMYwyZDhxstfTuQJAgPzuLDAYbtXSYbiTbhwJEMcwmZygCszCBZqkJySlAiKToceYXKfMOGPuWrkZUhWaUVRQjuLRlVPOGbWafMZLdDoJSFqqnTsApKVLlkuWgfGxuMbBINqUsjZvaPrNhktxHsWkqKWcVLqggOmaRWVanyriCrjMxNdwVROgsTmBZIanMXdlIBjbhghjHdtVCEjZIfxfhPAyrzxEXVazLpMUPQXCTDYjqyvikiLtZQAoHSBaCnUYMzSfYdiIURFqodOWYbQejuRHYnyFawEaEiXZaBcETbfXvUoswKDdbLFIZkpHYSqgCjYTeAzUtmHeaMSFIPdTeGEFAwccQQfmhVtbEagRmoEwnfkCmamjZKYwIznEifelnAPSWrAYBjTiTyJraPyjsNHsvPqRvEVznqUcydBvuonABuAtboWHKWdckmgGbnydnBRsmGQwlvPhazWcHFyAcgbACrjqbKlTMDxjPWMQGsoEEYOXeYUfAvwEmfgsqiNIHsdZsLMMRpZBUPbrqJlcxojxzZwgzXntTjFRPaVkpqYsOAfOVYBGnthxIAjHbwWebSPYmLDmsNdKBjeipZUZpnfDZtkwdyXhkIYjyNbwctJwPzTxkEAwLGOmkHkjKOzQtiIrJorXFNYqucCFIGSmMhDLAXmNkcEzjIvwizqYMVbAlBqcaqnCNRguHzzBKcXyIlBnzUoxCxpJYHWhaoErrIFGaVEdMhCVhlervzNeqjCwskPwHWCdYmOXwLGJncoxcdmlepBzIbuxEjVUzJsXCUGhSCYohoCLDFAfvRcRNLQxRLFRKZNiaLpQVogLlBMTccYBdPQoDFOZnwhxlktIDCwmZqVGkzuWmLhjpJroUZZrADUsWvdDKecKzWBkwZYgepuWGZBEPjHIDtGbwwogMWJdvfKDtapUxOsMxIAkwXJjIxxAVwxFSsFOApBdknfJIUKyKMENPiQZwCNhtPoRmnqLwkLAyPEkPOIPRFDeFymbdebcRDPyFXsXvjOByodxxoLDyzLoKtwDNMtpPqDoJZTGSvIFkFjRaSpgigOAxSXEKrQmzHOIbpjFLjECAuWLbhaSutNyZOyGnZGZeBkjdHUttkwpNMcOqLPGeEdLrrwluNbbDkdnSlcVXQMYwhRWgekeVXTdTOUSWxDKsXZhNvgTXTzgZEIiCBASOpzLBjYMASeOECCEQmBlfbETildpKfbhXsNiDjGALdXenMxCHjpgUYodykEyJuKtHlieZIhmOODNEySTEiaoToKrbaKNJyYyJrUuVyQoyoxeERHXWDQvIpMStOVoUHPktxkKsqNhnYfiydmLOQgUJmgDMBMDzAmAViFJjSHZpfInpqtMLDGhXfCGIMvjNQGUeBocEEUAhizHxnqapbQvVfkxdlsXjQDbWNibKmEtpcftdVxqEPSYVTsFDWDsbUfHthwyNFuGWxPWCbuLoekRLJIlWUVIuEPKbauDuLLuVymykVLkBnuSugIpkVkswwEaQISGinrKPMfkXiicyfEEsRdyaWSlUCSLaQdAWWYCLPoNnXuGIPtPQUAFitKaAvkPylCiupFRoNvmvJkeKmjnBMUjYcYRZPcvDnkFgqpBQUvWgmxsAeNNzpZPPTJwCIHHryFiXRXifJfCquTmXlMdArKoZreANlydvqldHhyYWWXGUmMJfZQVrmhILvYHNdzBgSbIGCkqMvStuLVtxZvNwkoUiizVAKRJhpftglROIpcUeXCdUihYmbSySdtxvVzMfWdOAFJkzggtAF"
+//Adding new line characters \\n
+//"ID_one" : "dHAndMGlIWfNcEXxxPhhIcjcDBJFzeXoUPVWoOZFAIZVVDmOnuRNNWacFmvOyAlCtUwSWPbrQjRixHsAHS\\nHiHvhalYMxoGmViDfWyylQbhkfLVOLVjwPQdANnZBrCIyVxHQHiDQXfTqQfTkfYUwhuRIGQzaukWVCZniofnqrJBvBVFhvEByOTteRXIbfXBAISVSUcNRZPXaTqoTJVFxWBXkypxvsq\\nRGKnfufQrtFSVuXsSYbDhPIuTRSEZEJvALbkyUtauFfvGoyYLnoEwUmJCHHOhpcYBQEfgAFAerenyCGnOktJNbnEZEMGmqKoozgq\\nIbtUwfGuJXdYIEhhxkxwTHacVugEVMkhKJJaPNgOJpODdMszSeMjwwIHXUWgwhbhwptljVptmeFmeDMrBndYVVlbifpElueFayYS\\nuJsAPZuGNCxFlJWdDdwxMmxzEcHNwvfFoXSJPolQgnxxNEJEVWCeMNkAhnnHkakHIfyVbhPHfOAEmjiZniZxAaETjpOrHZxMFgvdgmlcdEtKMn\\nTDDjaziWjYhRISEXBzudTJGFqlaUUFyIjezFoWDaQGhUGKtNWCtbTyUIZNcHTDWGZdPCtuYwtrwNIsPvVmaqSCyNlTpOQEwTRtAGXXiNcsXmp\\nKUyTjSYAvGLjLoTtcMVJzFjKktufNKuXACpxYReUiwjWoAnyytDparBWNAcqUCTqNKjqNfszoiTaCaPEnrnuVZhFiQruoNbrRNEQiefiiaQPfz\\nQisJfgdJjrbWbKRrZbhnNrrdrblVXEAwkftaYwWegnFuaJdNgiOYYgnuVWmwrSxhQZuHuizCGVfOetNKEvLkGahHPWrFPoGYvGWPhXhyEzs\\nKuBNQDJGqWenZbzMRLtogMDuZQXPORTZXrecTDNqqNruEbiLIReyPPAfKONzXfRandjbTBCXFnNhZLyChQixAkSuTOAcFXWKimlkZRDzAGqu\\nRwcuchClbmiFHfazKYCcMNpkZnXvhldRIrzlLxOpVqMZchnZmXPERlKwvRjJBeUxINkijjcvroPJyTCkZGnpMzKgSnXNmqTAFlUgSRpDTyCKcf\\nWMDqJaryVOTMRVdzOeWmldShlilsgGmtZHSGdpOYGdmFhWBXNTnjoTQeDeRHkasVHlaikXYWuyfBOijdMJYhRXfPXXqLtHYQBPxEEjKrW\\nqaHrDLTziRBMvznftcbmjkPBHYfhDDdmnsYGlrpzLJCznAUjvOjStfLRPrVbTtnAeNnbkzbtRvYnSnvUOgeDuINMYnpHmpkEFBdFPXnjGbvcoDa\\nUJXomVPTyHcxMcDGGXjjKHlPihjefvpQuhqrmVxpAjMjgzZGtEPJRGrkvblpIFHZAVgPaRQITjgkmhsbjooTyEGShCrblBur\\nubnJmgkKTTRGonzxikmgyxGXLxEdkuTNoyMggeObbvcQqDmovPLjohhXksNMpEvwWABaVmYqfmmaCLFHuboSVnmmbdQJEIVnhfAeUqwJjMjtg\\nOOfcafuddgiSxbavUVVEptTrltCPYuysHsBIXWjOrTEpHSDNsNZgKEgaqgsJUSaw\\npmCmkMaRrSRktotvGoSvWwDJMYRqQHpVLZRfdKgswCEZzvVZJtKexCbvFYdXISckfVbGbqBXNtDopMCTTosxogflBllsXIKuQhyRKGKFGRSFzFdI\\nbNjGVxfWBTGmeuFVaYvZkONXNtcJoMNxuiRPJKmlRfNkEIaaYaxdgCuHdQUcUGvDwiVbvdumYRgTTHlHkihXPdmPRlqEbiKeAZhQoyHVQXgWWFZ\\ncEUoeeMDSYNxWbNtvVzVmAnyvJJnYBkBOiCwlgWORxgBPrXevQlqebASiqnbTsdAAYuUTkASSvfSKxyBrIEAjuPbEINrOPDZDhhUeFuxaMHuJHTFP\\nHRHLOHiwWBkaNRKpDnLtDVqrMugqlspfAEXwyYtXdBgUhvXsKVkCWNvJregpjeHRBUcjQMPfOfONJXrfXUATxzUuJlDilXAHzmJVnMwKsyegHffggD\\nSNKAAmYpRWiRmzPpFucdeIrBCHNTurxKAJGGxDmjdoMeebbhTeBzMMEtPqwdqNCREjUuhhUPEgreUMyvQwhnKcADJxOmUzWwiklkyjVQWXuVxb\\n",
+//"ID_two" : "MLRlXfZhYiIAogbliTGlgrvysrSHSCdlTZamSutEgGZpNzfmhMqzBdieOygAkDPjmrojYlKXdEbKhnCYhD\\nMXAWCtmsRjNsGKlurfLJEuwhtIYOlTbBAyWshGUqbBZpPfPKKnDTAXWHRWlbVsNleTclmSYJqwxcPsrNYGtBJywlNWsjwfCtkkxnhdwwuxVMInwXKRUhyOhLNjomBppPPRvsgiIvhWz\\nkLfPZkdnzncAWzeyYYJLAurhCyrKHlEihekGOYopiyCyXzcLpGZqzDnToDCExPInuKZiOlTSVejqwXnhElOPwEnnecLflfzKYGRY\\nOhiFlbgCtQGLReFWegAoWMYwyZDhxstfTuQJAgPzuLDAYbtXSYbiTbhwJEMcwmZygCszCBZqkJySlAiKToceYXKfMOGPuWrkZUhW\\naUVRQjuLRlVPOGbWafMZLdDoJSFqqnTsApKVLlkuWgfGxuMbBINqUsjZvaPrNhktxHsWkqKWcVLqggOmaRWVanyriCrjMxNdwVROgsTmBZIanM\\nXdlIBjbhghjHdtVCEjZIfxfhPAyrzxEXVazLpMUPQXCTDYjqyvikiLtZQAoHSBaCnUYMzSfYdiIURFqodOWYbQejuRHYnyFawEaEiXZaBcETb\\nfXvUoswKDdbLFIZkpHYSqgCjYTeAzUtmHeaMSFIPdTeGEFAwccQQfmhVtbEagRmoEwnfkCmamjZKYwIznEifelnAPSWrAYBjTiTyJraPyjsNHs\\nvPqRvEVznqUcydBvuonABuAtboWHKWdckmgGbnydnBRsmGQwlvPhazWcHFyAcgbACrjqbKlTMDxjPWMQGsoEEYOXeYUfAvwEmfgsqiNIHsd\\nZsLMMRpZBUPbrqJlcxojxzZwgzXntTjFRPaVkpqYsOAfOVYBGnthxIAjHbwWebSPYmLDmsNdKBjeipZUZpnfDZtkwdyXhkIYjyNbwctJwPzT\\nxkEAwLGOmkHkjKOzQtiIrJorXFNYqucCFIGSmMhDLAXmNkcEzjIvwizqYMVbAlBqcaqnCNRguHzzBKcXyIlBnzUoxCxpJYHWhaoErrIFGaVEdM\\nhCVhlervzNeqjCwskPwHWCdYmOXwLGJncoxcdmlepBzIbuxEjVUzJsXCUGhSCYohoCLDFAfvRcRNLQxRLFRKZNiaLpQVogLlBMTccYBdP\\nQoDFOZnwhxlktIDCwmZqVGkzuWmLhjpJroUZZrADUsWvdDKecKzWBkwZYgepuWGZBEPjHIDtGbwwogMWJdvfKDtapUxOsMxIAkwXJjIxxAVwxFS\\nsFOApBdknfJIUKyKMENPiQZwCNhtPoRmnqLwkLAyPEkPOIPRFDeFymbdebcRDPyFXsXvjOByodxxoLDyzLoKtwDNMtpPqDoJ\\nZTGSvIFkFjRaSpgigOAxSXEKrQmzHOIbpjFLjECAuWLbhaSutNyZOyGnZGZeBkjdHUttkwpNMcOqLPGeEdLrrwluNbbDkdnSlcVXQMYwhRWge\\nkeVXTdTOUSWxDKsXZhNvgTXTzgZEIiCBASOpzLBjYMASeOECCEQmBlfbETildpKf\\nbhXsNiDjGALdXenMxCHjpgUYodykEyJuKtHlieZIhmOODNEySTEiaoToKrbaKNJyYyJrUuVyQoyoxeERHXWDQvIpMStOVoUHPktxkKsqNhnYfiyd\\nmLOQgUJmgDMBMDzAmAViFJjSHZpfInpqtMLDGhXfCGIMvjNQGUeBocEEUAhizHxnqapbQvVfkxdlsXjQDbWNibKmEtpcftdVxqEPSYVTsFDWDsb\\nUfHthwyNFuGWxPWCbuLoekRLJIlWUVIuEPKbauDuLLuVymykVLkBnuSugIpkVkswwEaQISGinrKPMfkXiicyfEEsRdyaWSlUCSLaQdAWWYCLPoNnX\\nuGIPtPQUAFitKaAvkPylCiupFRoNvmvJkeKmjnBMUjYcYRZPcvDnkFgqpBQUvWgmxsAeNNzpZPPTJwCIHHryFiXRXifJfCquTmXlMdArKoZreANlyd\\nvqldHhyYWWXGUmMJfZQVrmhILvYHNdzBgSbIGCkqMvStuLVtxZvNwkoUiizVAKRJhpftglROIpcUeXCdUihYmbSySdtxvVzMfWdOAFJkzggtAF\\n"
+
+// Array to store matched indices
+int matchedIndices[2130];
+int numMatches = 0; // Counter for the number of matches
+byte SensorData[1536];
+
+int X = 0;
+int Y = 1;
+int Z = 2;
+void setup() {
+  Serial.begin(115200);
+  delay(2000);
+
+  //Print new line characters '\n' by given instructions
+  //  Serial.print("\nString before replacement: ");
+  //  Serial.println(array1);
+  array1.replace("\\n", "");
+
+  //  Serial.print("\nString After replacement:  ");
+  //  Serial.println(array1);
+
+  // Compare arrays and store matched indices
+  for (int i = 0; i < array1.length(); i++) {
+    for (int j = 0; j < strlen(array2); j++) {
+      if (array1.charAt(i) == array2[j]) {
+        matchedIndices[numMatches] = j;
+        numMatches++;
+      }
+    }
+  }
+
+  // Print the matched indices
+  //  Serial.print("Matched Indices: ");
+  //  for (int i = 0; i < numMatches; i++) {
+  //    Serial.print(i);
+  //    Serial.print(":");
+  //    Serial.print(matchedIndices[i], HEX);
+  //    Serial.print(", ");
+  //  }
+
+
+  //Process 4 bytes for testing
+  //    byte FourBytes[4];
+  //    for (int i = 0; i < 5; i++) {
+  //      FourBytes[i] = matchedIndices[i];
+  //    }
+  //    Base64(FourBytes);
+
+  //Process full array
+  int arraySize = sizeof(matchedIndices) / sizeof(matchedIndices[0]);
+  for (int i = 0; i < arraySize; i += 4) {
+    byte group[4];
+    for (int j = 0; j < 4 && i + j < arraySize; j++) {
+      group[j] = matchedIndices[i + j];
+    }
+    Base64(group);
+  }
+
+  Serial.println();
+  for (int i = 0; i < sizeof(SensorData); i++)
+  {
+    Serial.print("0x");
+    Serial.print(SensorData[i], HEX);
+    Serial.print(",");
+  }
+  Serial.println();
+  numMatches = 0;
+  X = 0;
+  Y = 1;
+  Z = 2;
+
+  array3.replace("\\n", "");
+
+  for (int i = 0; i < array3.length(); i++) {
+    for (int j = 0; j < strlen(array2); j++) {
+      if (array3.charAt(i) == array2[j]) {
+        matchedIndices[numMatches] = j;
+        numMatches++;
+      }
+    }
+  }
+
+  // Print the matched indices
+  //  Serial.print("Matched Indices: ");
+  //  for (int i = 0; i < numMatches; i++) {
+  //    Serial.print(i);
+  //    Serial.print(":");
+  //    Serial.print(matchedIndices[i], HEX);
+  //    Serial.print(", ");
+  //  }
+
+  arraySize = sizeof(matchedIndices) / sizeof(matchedIndices[0]);
+  for (int i = 0; i < arraySize; i += 4) {
+    byte group[4];
+    for (int j = 0; j < 4 && i + j < arraySize; j++) {
+      group[j] = matchedIndices[i + j];
+    }
+    Base64(group);
+  }
+
+  Serial.println();
+  for (int i = 0; i < sizeof(SensorData); i++)
+  {
+    Serial.print("0x");
+    Serial.print(SensorData[i], HEX);
+    Serial.print(",");
+  }
+  Serial.println();
+
+  numMatches = 0;
+  X = 0;
+  Y = 1;
+  Z = 2;
+}
+
+void Base64(byte FourBytes[]) {
+  
+  // Step 1: Remove 2 prefix bits, create 4 sets of 6 bits, and concatenate into a 24-bit variable
+  unsigned long concatenatedValue = 0;
+  for (int i = 0; i < 4; i++) {
+    // Remove 2 prefix bits
+    byte sixBits = FourBytes[i] & 0b00111111;
+    concatenatedValue <<= 6; // Shift left by 6 bits
+    concatenatedValue |= sixBits;
+  }
+
+  // Step 2: Separate the 24-bit variable into 3 bytes
+  byte byte1 = (concatenatedValue >> 16) & 0xFF;
+  byte byte2 = (concatenatedValue >> 8) & 0xFF;
+  byte byte3 = concatenatedValue & 0xFF;
+
+  //Print each byte with its index number
+  //  Serial.print("\nByte ");
+  //  Serial.print(X);
+  //  Serial.print(": ");
+  //  Serial.println(byte1, HEX);
+  //
+  //  Serial.print("Byte ");
+  //  Serial.print(Y);
+  //  Serial.print(": ");
+  //  Serial.println(byte2, HEX);
+  //
+  //  Serial.print("Byte ");
+  //  Serial.print(Z);
+  //  Serial.print(": ");
+  //  Serial.println(byte3, HEX);
+  //  Serial.print("---------------------------");
+
+  SensorData[X] = byte1;
+  SensorData[Y] = byte2;
+  SensorData[Z] = byte3;
+  X += 3;
+  Y += 3;
+  Z += 3;
+}
+
+void loop() {
+  // Your code here (if needed)
+}
